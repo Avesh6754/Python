24bec6dabec825dff0cfa4adb4d1f15a6764dd7a
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-05-24 12:47:03 +0530

    Update Pattern11.py

--- Python/Pattern11.py
+++ Python/Pattern11.py
@@ -3,9 +3,9 @@ j=9
 
 for i in range(1,6):
     for k in range(1,(6-i)+1):
-        print(end=" ")
-    for j in range(1,9):
-        if (j==1 or i==5 or j==8):
+        print(end="  ")
+    for j in range(1,9+1):
+        if (j==1 or i==5 or i==2 and j>2 and j<4 or i==3 and j>4 and j<6 or i==4 and j>6 and j<8):
             print("*",end=" ")
         else:
             print(" ",end=" ")
