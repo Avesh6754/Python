cef10fbaff2ef9e5513d28d5500e15c07afb0bed
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-05-24 12:47:31 +0530

    Add files via upload

---
+++ Python/Pattern12.py
@@ -0,0 +1,13 @@
+i=6
+j=9
+
+for i in range(1,6):
+    for k in range(1,(6-i)+1):
+        print(end="  ")
+    for j in range(1,9+1):
+        if (j==1 or i==5 or i==2 and j>2 and j<4 or i==3 and j>4 and j<6 or i==4 and j>6 and j<8):
+            print(j,end=" ")
+        else:
+            print(" ",end=" ")
+      
+    print()
