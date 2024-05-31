9e88ef537a0a2e658e6ae846926b00a9103c15e5
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-05-31 15:08:36 +0530

    Update file_not_found.py

--- Python/file_not_found.py
+++ Python/file_not_found.py
@@ -1,9 +1,11 @@
 file_name=input("Enter the data : ")
 
 try:
-    file= open(file_name,"r")
-    file.read()
-    print(file_name)
+    file= open("demo3.txt","w")
+    file.write(file_name)
     file.close()
+    file= open("demo2.txt","r")
+    print(file.read())
+   
 except:
     print("File Not Found :")
