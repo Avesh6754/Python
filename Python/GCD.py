6c8f83effbb4e5139f4e7665d45561b578e8222c
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-05-31 14:45:26 +0530

    Update GCD.py

--- Python/GCD.py
+++ Python/GCD.py
@@ -1,9 +1,15 @@
-def fun(a,b):
-    while b!=0:
-        a=b
-        b=(a%b)
-    return a
 
+GCD=0
 a=int(input("Enter the value of a : "))
 b=int(input("Enter the value of b : "))
-print(fun(a,b))
+smaller=0
+
+if a>b:
+    smaller=b
+else:
+    smaller=a
+
+for i in range(1,smaller+1):
+    if a%i==0 and b%i==0:
+            GCD=i
+print(GCD)
